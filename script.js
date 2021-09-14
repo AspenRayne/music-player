3ac1c35f082dc71e912dc65a79151ec1fd9a193e
AspenRayne <90653438+AspenRayne@users.noreply.github.com>
2021-09-14 16:13:01 -0600

    music player that allows you to change song and volume. Also has a animation picture that spins when the music is playing.

--- script.js
+++ script.js
@@ -7,9 +7,10 @@ const progress = document.querySelector('.progress')
 const progressContainer = document.querySelector('.progress-container')
 const title = document.querySelector('#title')
 const cover = document.querySelector('#cover')
+// const volume = document.querySelector
 
 // Song Title
-const songs = ['hey', 'summer', 'ukulele']
+const songs = ['dubstep', 'betterdays', 'onceagain']
 
 // Keep track of songs
 let songIndex = 2
@@ -21,21 +22,61 @@ loadSong(songs[songIndex])
 function loadSong(song) {
     title.innerText = song
     audio.src = `music/${song}.mp3`
-    cover.src = `images/${song}.jpg`
+    cover.src = `images/${song}.jpg` 
 }
 
 function playSong() {
     musicContainer.classList.add('play');
     playBtn.querySelector('i.fas').classList.remove('fa-play')
     playBtn.querySelector('i.fas').classList.add('fa-pause')
+
+    audio.play()
 }
 
 function pauseSong() {
     musicContainer.classList.remove('play');
     playBtn.querySelector('i.fas').classList.add('fa-play')
     playBtn.querySelector('i.fas').classList.remove('fa-pause')
+
+    audio.pause()
 }
 
+function prevSong(){
+    songIndex--
+
+    if(songIndex < 0){
+        songIndex = songs.length - 1
+    }
+    loadSong(songs[songIndex])
+
+    playSong()
+
+}
+
+function nextSong(){
+    songIndex++
+
+    if(songIndex > songs.length - 1) {
+        songIndex = 0 
+    }
+    loadSong(songs[songIndex])
+
+    playSong()
+}
+
+function updateProgress(e) {
+    const {duration, currentTime} = e.srcElement
+    const progressPercent = (currentTime / duration) * 100
+    progress.style.width = `${progressPercent}%`
+}
+
+function setProgress(e) {
+    const width = this.clientWidth
+    const clickX = e.offsetX
+    const duration = audio.duration
+
+    audio.currentTime = (clickX / width) * duration
+}
 // Event listeners
 playBtn.addEventListener('click' , () => {
     const isPlaying = musicContainer.classList.contains('play')
@@ -46,3 +87,27 @@ playBtn.addEventListener('click' , () => {
         playSong()
     }
 })
+
+// Change song event
+prevBtn.addEventListener('click', prevSong)
+nextBtn.addEventListener('click', nextSong)
+
+audio.addEventListener('timeupdate', updateProgress)
+
+progressContainer.addEventListener('click', setProgress)
+
+audio.addEventListener('ended', nextSong)
+
+//Change Volume
+document.addEventListener("DOMContentLoaded", () => {
+    const range = document.querySelector(".volume input[type=range]");
+  
+    const barHoverBox = document.querySelector(".volume .bar-hoverbox");
+    const fill = document.querySelector(".volume .bar .bar-fill");
+    
+    range.addEventListener("change", (e) => {
+        audio.volume = e.target.value;
+    //   console.log("value", e.target.value);
+    });
+});
+    
